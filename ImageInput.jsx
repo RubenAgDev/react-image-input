8bfc5e45fef7e765afab4d16c6c9d9425c0174f1
Ruben Aguilar <rube.a87@gmail.com>
2018-08-14 15:37:31 -0700

    Add files via upload

---
+++ ImageInput.jsx
@@ -0,0 +1,135 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helpers from '../utils/Helpers';
+
+import '../assets/css/ImageInput.css';
+
+const DEFAULT_SIZE = 150;
+const EMPTY_OBJECT = {};
+
+class ImageInput extends React.Component {
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            image: props.defaultValue
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleClearClick = this.handleClearClick.bind(this);
+        this.saveRef = this.saveRef.bind(this);
+
+        if (this.props.maxSize) {
+            this.size = {
+                width: this.props.maxSize.width,
+                height: this.props.maxSize.height
+            };
+        } else {
+            this.size = {
+                width: DEFAULT_SIZE,
+                height: DEFAULT_SIZE
+            };
+        }
+    }
+
+    static getDerivedStateFromProps(props, state) {
+        if (props.value && state !== props.value) {
+            return {
+                image: props.value
+            };
+        }
+
+        return null;
+    }
+
+    handleChange() {
+        const file = this.fileInput.files[0];
+        const reader = new FileReader();
+
+        reader.onload = (e) => {
+            const image = e.target.result;
+
+            if (this.props.maxSize) {
+                Helpers.resizeImage(image, this.props.maxSize.width, this.props.maxSize.height).then((newImage) => {
+                    this.updateImage(newImage);
+                });
+            } else {
+                this.updateImage(image);
+            }
+        };
+
+        if (file) {
+            reader.readAsDataURL(file);
+        }
+    }
+
+    handleClearClick() {
+        this.fileInput.value = '';
+        this.setState({
+            image: ''
+        });
+        if (this.props.onChange) {
+            this.props.onChange('');
+        }
+    }
+
+    saveRef(input) {
+        this.fileInput = input;
+    }
+
+    updateImage(image) {
+        this.setState({
+            image
+        }, () => {
+            if (this.props.onChange) {
+                this.props.onChange(image);
+            }
+        });
+    }
+
+    render() {
+        return (
+            <div className='amber-image-input'>
+                <label>
+                    {
+                        (this.props.placeholder && !this.state.image) ?
+                            <div className='default-content' style={ this.size }>{ this.props.placeholder }</div> :
+                            <img 
+                                alt={this.props.description} 
+                                src={this.state.image}
+                                style={this.state.image ? EMPTY_OBJECT : this.size }
+                                title={this.props.title} />
+                    }
+                    <input
+                        accept='image/*'
+                        disabled={this.props.disabled}
+                        ref={this.saveRef}
+                        type='file'
+                        onChange={this.handleChange} />    
+                </label>
+                <div className='clear' onClick={this.handleClearClick} title='Remove image'>X</div>
+            </div>
+        );
+    }
+}
+
+ImageInput.propTypes = {
+    /** Optional (alt) attribute to be used in a SimpleFormItem */
+    description: PropTypes.string,
+    /** Default image to be loaded */
+    defaultValue: PropTypes.string,
+    disabled: PropTypes.bool,
+    /** Max size of the selected image. The image will be resized if exedes the dimensions */
+    maxSize: PropTypes.shape({
+        width: PropTypes.number,
+        height: PropTypes.number,
+    }),
+    /** Optional default content to be displayed when there is no value */
+    placeholder: PropTypes.node,
+    /** Callback for the onchange event */
+    onChange: PropTypes.func,
+    /** Title attribute for the image */
+    title: PropTypes.string,
+    value: PropTypes.string,
+};
+
+export default ImageInput;
---
+++ ImageInput.md
@@ -0,0 +1,4 @@
+Example:
+```js
+<ImageInput placeholder='Click here to select an image from your files' maxSize={{width: 200, height: 200}} />
+```
